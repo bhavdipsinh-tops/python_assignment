28866e9b8aad6a6b085bc8c5b1ddbe51b5496f8f
bhavdipsinh-tops <119838762+bhavdipsinh-tops@users.noreply.github.com>
2023-07-10 12:48:58 +0530

    Add files via upload

---
+++ Module – 5 (DB and Python Framework)/Explain how you create project django.py
@@ -0,0 +1,25 @@
+'''Ans:=
+    Django is a high-level Python web framework that enables rapid development
+    of secure and maintainable websites. Built by experienced developers,
+    Django takes care of much of the hassle of web development,
+    so you can focus on writing your app without needing to reinvent the wheel
+    '''
+
+
+#C:\Users\91814>cd "OneDrive\Desktop\python\Final Project"
+
+# >python -m venv myvenv
+
+# >myvenv\scripts\activate
+
+# >pip install django
+
+# >django-admin startproject mysite
+
+# >django-admin startapp myapp
+
+# >python manage.py migrate
+
+# >python manage.py createsuperuser
+
+# >python manage.py runserver
